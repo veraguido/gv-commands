d19feaea1a57c5b4e15b8e39b85bdbf827afcf5b
guido <guido@pop-os.localdomain>
2021-06-15 13:52:09 +0100

    first commit

---
+++ src/Commands/CommandInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace Gvera\Commands;
+
+/**
+ * Command Class Doc Comment
+ *
+ * @category Class
+ * @package  src/commands
+ * @author    Guido Vera
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.github.com/veraguido/gv
+ *
+ */
+interface CommandInterface
+{
+    public function execute();
+}
